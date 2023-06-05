05d35ad4cb454f7f70c1a75bfe6e779479538dc1
2194063 <jeromegarcia.16@gmail.com>
2023-06-05 20:11:13 +0800

    First Commit

--- src/main/java/com/masterclass/employee/directory/Application.java
+++ src/main/java/com/masterclass/employee/directory/Application.java
@@ -3,11 +3,25 @@ package com.masterclass.employee.directory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+
+		Scanner scan = new Scanner(System.in);
+		int input;
+		System.out.println("Main Option\n" +
+				"[1] List All Employee Records\n" +
+				"[2] Add New Employee Record\n" +
+				"[3] Delete Employee Record\n" +
+				"[4] Search Employee Record\n" +
+				"[-1] exit\n");
+		System.out.print("Enter action type: ");
+		input = scan.nextInt();
+
+
 	}
 
 }
