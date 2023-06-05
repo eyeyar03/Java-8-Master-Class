aa801873c61a0afc7132e38fc44a1c0c744dbc9f
Alinsangan <2236569@cognizant.com>
2023-06-05 17:57:11 +0800

    First Commit

---
+++ src/test/java/com/masterclass/employee/directory/SortEmployeeTest.java
@@ -0,0 +1,7 @@
+package com.masterclass.employee.directory;
+
+import com.masterclass.employee.directory.service.CommandAction;
+
+public class SortEmployeeTest {
+    private CommandAction sortByEmpNumber;
+}
